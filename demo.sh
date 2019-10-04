38fb9fc5e8a5f6e185ca3477d3ad9895083c91e8
Thomas Uphill <thomas@narrabilis.com>
2019-10-04 11:52:19 -0700

    tmux session

--- demo.sh
+++ demo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # start tmux first
-tmux new-session -s lisa
+tmux new-session -c /home/thomas/lisa2019 -s lisa
+tmux neww -n glibc
+tmux neww -n vagrant
 
 # start ttyd
 ttyd -p 8081 -t fontSize=24 tmux attach -t lisa 2>&1 >/tmp/ttyd &

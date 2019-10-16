afd88c3e082ba2120912821934735162fa900c53
Thomas Uphill <thomas@narrabilis.com>
2019-10-16 09:28:53 -0700

    ps with grep

--- ansible/getip.sh
+++ ansible/getip.sh
@@ -4,3 +4,8 @@ alias ap='asciinema play'
 alias psx="(ps -eo \"%P %p %c\" |awk 'NR==1' && ps -eo \"%P %p %c\" | grep -v PPID|sort -n) |less"
 alias psk="(ps aux | awk 'NR==1' && ps aux |grep -E \\\\[\|\\\\] |grep -vE grep\|sshd) |less"
 alias brc=". ~/.bashrc && echo reread .bashrc"
+function psg() {
+  thing=$1
+  ps -eo "%P %p %c %a" |awk 'NR==1'
+  ps -eo "%P %p %c %a" |grep -w $thing |grep -v grep
+}

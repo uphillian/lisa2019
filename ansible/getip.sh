2e3f979bf8b24a38fcd1e2576c6a1de7eaa12ce8
Thomas Uphill <thomas@narrabilis.com>
2019-10-25 10:43:04 -0700

    gdb example

--- ansible/getip.sh
+++ ansible/getip.sh
@@ -1,11 +1,13 @@
 export PATH=$PATH:/usr/local/bin
 alias ar='asciinema rec'
-alias ap='asciinema play'
-alias psx="(ps -eo \"%P %p %c\" |awk 'NR==1' && ps -eo \"%P %p %c\" | grep -v PPID|sort -n) |less"
+alias psx="(ps -eo \"ppid pid stat cmd\" |awk 'NR==1' && ps -eo \"ppid pid stat cmd\" | grep -v PPID|sort -n) |less"
 alias psk="(ps aux | awk 'NR==1' && ps aux |grep -E \\\\[\|\\\\] |grep -vE grep\|sshd) |less"
 alias brc=". ~/.bashrc && echo reread .bashrc"
 function psg() {
   thing=$1
   ps -eo "ppid pid stat cmd" |awk 'NR==1'
   ps -eo "ppid pid stat cmd" |grep -w $thing |grep -v grep
+}
+function ap() {
+  echo $@ | xargs -n1 asciinema play
 }
